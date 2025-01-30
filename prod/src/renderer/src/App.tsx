5a50587dc6c9d395ac2fbdc6434e77c3c8ea71a9
charlieCompton <ccompton@lclark.edu>
2025-01-30 09:22:38 -0500

    gixed a typo (haha get it?) with pageS, and then switched to HashRouter instead of BrowserRouter

--- prod/src/renderer/src/App.tsx
+++ prod/src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import MainPage from "./components/AppLayout"
 import SquareNote from "./components/SquareNote"
 
@@ -9,7 +9,7 @@ function App(): JSX.Element {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />}/>
-        <Route path="/page/:squareId" element={<SquareNote />}/>
+        <Route path="/pages/:squareId" element={<SquareNote />}/>
       </Routes>
     </Router>
     {/* <div className="flex h-full items-center justify-center">
--- prod/src/renderer/src/components/SquareNote.tsx
+++ prod/src/renderer/src/components/SquareNote.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const SquareNote = () => {
     let {squareId} = useParams();
+
+    useEffect(() => {
+        console.log("Rendering SquareNote:", squareId);
+    }, [squareId]);
 
     return(
         <div className="h-100 w-100 flex items-center justify-center">
